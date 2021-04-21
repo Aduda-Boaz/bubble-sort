a28693a3a0860a39be6ea65389eeaaa1c4b08237
panda4cs <panda4cs@gmail.com>
2021-04-21 22:59:06 +0200

    Solved second section of the problem

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,6 +1,6 @@
 numbers = [4,3,78,2,0,2] #nil
 
-def bubble_sort (array)
+def bubble_sort(array)
   is_sorted = false
   while  !is_sorted
     is_sorted = swap_if_bigger (array)
@@ -8,7 +8,7 @@ def bubble_sort (array)
   puts array
 end
 
-def swap_if_bigger (array)
+def swap_if_bigger(array)
   is_sorted = true
   (array.size - 1).times do |i|
     if (array[i] > array[i + 1])
@@ -19,6 +19,18 @@ def swap_if_bigger (array)
   return is_sorted 
 end
 
-
 # swap_if_bigger(numbers)
-bubble_sort (numbers)
+# bubble_sort(numbers)
+
+def bubble_sort_by(array)
+  (array.size - 1).times do |i|
+    if ((yield array[i], array[i + 1]) > 0)
+      array[i], array[i + 1] = array[i + 1], array[i]
+    end
+  end
+  puts array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
