b6741647b7ebba9ba809322f8fdec0a4180ec12b
panda4cs <panda4cs@gmail.com>
2021-04-22 00:20:27 +0200

    Fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,21 +1,22 @@
-numbers = [4,3,78,2,0,2] 
+numbers = [4, 3, 78, 2, 0, 2]
 
 def bubble_sort(array)
   is_sorted = false # a condition to return false if array is sorted
-  while  !is_sorted
-    is_sorted = swap_if_bigger(array) # if not sorted, we swap 
+  until is_sorted
+    is_sorted = swap_if_bigger(array) # if not sorted, we swap
   end
+  return array
 end
 
-def swap_if_bigger (array)
-  is_sorted = true 
-  (array.size - 1).times do |i| 
-    if (array[i] > array[i + 1])
+def swap_if_bigger(array)
+  is_sorted = true
+  (array.size - 1).times do |i|
+    if array[i] > array[i + 1]
       is_sorted = false
       array[i], array[i + 1] = array[i + 1], array[i]
     end
   end
-  return is_sorted
+  is_sorted
 end
 
 swap_if_bigger(numbers)
@@ -23,7 +24,7 @@ bubble_sort(numbers)
 
 def bubble_sort_by(array)
   (array.size - 1).times do |i|
-    if ((yield array[i], array[i + 1]) > 0)
+    if (yield array[i], array[i + 1]).positive?
       array[i], array[i + 1] = array[i + 1], array[i]
     end
   end
@@ -33,3 +34,5 @@ end
 bubble_sort_by(["hi","hello","hey"]) do |left,right|
   left.length - right.length
 end
+
+puts bubble_sort(numbers)
