6bf7f70578ea2dcf000bd356263e47e16ec4a579
Aduda-Boaz <adudaboaz@gmail.com>
2021-04-22 00:56:21 +0300

    Fix rubocop errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,14 @@
-numbers = [4,3,78,2,0,2] 
-
-def bubble_sort (array)
+def bubble_sort(array)
   is_sorted = false # a condition to return false if array is sorted
-  while  !is_sorted
-    is_sorted = swap_if_bigger (array) # if not sorted, we swap 
+  until !is_sorted
+    is_sorted = swap_if_bigger(array) # if not sorted, we swap 
   end
   puts array
 end
 
-def swap_if_bigger (array)
-  is_sorted = true 
-  (array.size - 1).times do |i| 
-    if (array[i] > array[i + 1])
+def swap_if_bigger(array)
+  is_sorted = true  (array.size - 1).times do |i| 
+    if array[i] > array[i + 1]
       is_sorted = false
       array[i], array[i + 1] = array[i + 1], array[i]
     end
@@ -20,17 +17,17 @@ def swap_if_bigger (array)
 end
 
 # swap_if_bigger(numbers)
-# bubble_sort(numbers)
+bubble_sort([4, 3, 78, 2, 0, 2])
 
 def bubble_sort_by(array)
   (array.size - 1).times do |i|
-    if ((yield array[i], array[i + 1]) > 0)
+    if (yield array[i], array[i + 1]).positive?
       array[i], array[i + 1] = array[i + 1], array[i]
     end
   end
   puts array
 end
 
-bubble_sort_by(["hi","hello","hey"]) do |left,right|
+bubble_sort_by(['hi', 'hello', 'hey']) do |left, right|
   left.length - right.length
 end
