8882a37670ba22119ff1bd8a5ca185d0f16e5214
panda4cs <panda4cs@gmail.com>
2021-04-22 17:05:41 +0200

    Fix linters errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -31,9 +31,8 @@ arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 p bubble_sort(arr)
 p bubble_sort(numbers)
 
-arr = bubble_sort_by(["h", "hi", "hey", "hiya", "hello"]) do |left, right|
-left.length - right.length
+arr = bubble_sort_by(%w[h hi hey hiya hello]) do |left, right|
+  left.length - right.length
 end
 
 p arr
-
